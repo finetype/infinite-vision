2661c3e3703121b57c86f2fdb7d4aba990a06fe0
Kyle Baker <finesttype@gmail.com>
2015-05-28 23:26:29 -0500

    more navbar fixes

--- renderings.js
+++ renderings.js
@@ -1,28 +1,32 @@
-/* activate the carousel */
-$("#modal-carousel").carousel({interval:false});
+$(document).ready(function(){
 
-/* change modal title when slide changes */
-$("#modal-carousel").on("slid.bs.carousel", function () {
-  $(".modal-title").html($(this).find(".active img").attr("title"));
-})
+  /* activate the carousel */
+  $("#modal-carousel").carousel({interval:false});
 
-/* when clicking a thumbnail */
-$(".row .thumbnail").click(function(){
-    var content = $(".carousel-inner");
-    var title = $(".modal-title");
-  
-    content.empty();  
-    title.empty();
-  
-  	var id = this.id;  
-    var repo = $("#img-repo .item");
-    var repoCopy = repo.filter("#" + id).clone();
-    var active = repoCopy.first();
-  
-    active.addClass("active");
-    title.html(active.find("img").attr("title"));
-  	content.append(repoCopy);
+  /* change modal title when slide changes */
+  $("#modal-carousel").on("slid.bs.carousel", function () {
+    $(".modal-title").html($(this).find(".active img").attr("title"));
+  })
 
-    // show the modal
-  	$("#modal-gallery").modal("show");
+  /* when clicking a thumbnail */
+  $(".row .thumbnail").click(function(){
+      var content = $(".carousel-inner");
+      var title = $(".modal-title");
+    
+      content.empty();  
+      title.empty();
+    
+    	var id = this.id;  
+      var repo = $("#img-repo .item");
+      var repoCopy = repo.filter("#" + id).clone();
+      var active = repoCopy.first();
+    
+      active.addClass("active");
+      title.html(active.find("img").attr("title"));
+    	content.append(repoCopy);
+
+      // show the modal
+    	$("#modal-gallery").modal("show");
+  });
+
 });
